c17729087cd9845af15789a86a33a3a020268be1
NicolasPalsa <s2401220@edu.bc.fi>
2026-04-17 10:38:55 +0300

    improved database logic

--- dbscripts/create-db.sql
+++ dbscripts/create-db.sql
@@ -11,8 +11,8 @@ CREATE TABLE server (
    private TINYINT NOT NULL
 ) ENGINE=InnoDB;
 
-DROP TABLE IF EXISTS user;
-CREATE TABLE user (
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
 	user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	full_name VARCHAR(100) NOT NULL,
 	username VARCHAR(100) UNIQUE NOT NULL,
@@ -24,8 +24,8 @@ CREATE TABLE user (
 	blacklist TINYINT NOT NULL,
 	servers_created TINYINT NOT NULL,
 	servers_joined TINYINT NOT NULL,
-	status TINYTEXT NOT NULL,
-	avatar_url TEXT NOT NULL,
+	status VARCHAR(255) NOT NULL,
+	avatar_url VARCHAR(255) NOT NULL,
 	bio TEXT NOT NULL
 ) ENGINE=InnoDB;
 
@@ -37,7 +37,7 @@ CREATE TABLE friend_list (
 	friends_since DATETIME NULL,
 	
 	FOREIGN KEY (friend_list_id)
-	REFERENCES user (user_id)
+	REFERENCES users (user_id)
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS block_list;
@@ -46,26 +46,28 @@ CREATE TABLE block_list (
 	user_id INT NOT NULL,
 	
 	FOREIGN KEY (block_list_id)
-	REFERENCES user (user_id)
+	REFERENCES users (user_id)
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS  dms;
 CREATE TABLE dms (
-	dm_id INT UNIQUE NOT NULL PRIMARY KEY,
+	dm_id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	user_1 INT NOT NULL,
 	user_2 INT NOT NULL,
 	creation_date DATETIME NOT NULL,
+
+    UNIQUE (user_1, user_2),
 	
 	FOREIGN KEY (user_1)
-	REFERENCES user (user_id),
+	REFERENCES users (user_id),
 	
 	FOREIGN KEY (user_2)
-	REFERENCES user (user_id)
+	REFERENCES users (user_id)
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS dm_messages;
 CREATE TABLE dm_messages (
-	message_id INT UNIQUE NOT NULL PRIMARY KEY,
+	message_id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	dm_id INT NOT NULL,
 	user_id INT NOT NULL,
 	message MEDIUMTEXT NOT NULL,
@@ -75,11 +77,12 @@ CREATE TABLE dm_messages (
 	REFERENCES dms (dm_id),
 	
 	FOREIGN KEY (user_id)
-	REFERENCES USER (user_id)
+	REFERENCES users (user_id)
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS ban_list;
 CREATE TABLE ban_list (
+    ban_list_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	server_id INT NOT NULL,
 	user_id INT NOT NULL,
 	ban_duration DATETIME NULL,
@@ -88,11 +91,12 @@ CREATE TABLE ban_list (
 	REFERENCES server (server_id),
 	
 	FOREIGN KEY (user_id)
-	REFERENCES user (user_id)
+	REFERENCES users (user_id)
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS member_list;
 CREATE TABLE member_list (
+    member_list_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	server_id INT NOT NULL,
 	user_id INT NOT NULL,
 	owner TINYINT NOT NULL,
@@ -103,12 +107,12 @@ CREATE TABLE member_list (
 	REFERENCES server (server_id),
 	
 	FOREIGN KEY (user_id)
-	REFERENCES user (user_id)
+	REFERENCES users (user_id)
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS channel;
 CREATE TABLE channel (
-	channel_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	channel_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	server_id INT NOT NULL,
 	name VARCHAR(50) NOT NULL,
 	private TINYINT NOT NULL,
@@ -120,7 +124,7 @@ CREATE TABLE channel (
 
 DROP TABLE IF EXISTS channel_messages;
 CREATE TABLE channel_messages (
-	message_id INT UNIQUE NOT NULL PRIMARY KEY,
+	message_id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	channel_id INT NOT NULL,
 	user_id INT NOT NULL,
 	message MEDIUMTEXT NOT NULL,
@@ -130,6 +134,6 @@ CREATE TABLE channel_messages (
 	REFERENCES channel (channel_id),
 	
 	FOREIGN KEY (user_id)
-	REFERENCES USER (user_id)
+	REFERENCES users (user_id)
 ) ENGINE=InnoDB;
 
